75d5510cd51037fcdca78837c1cb470d117a04e5
YH CHEN <realcyh@outlook.com>
2019-09-20 23:13:33 -0400

    Initial Commit

---
+++ kmeans/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  kmeans
+//
+//  Created by REAL CYH on 9/20/19.
+//  Copyright © 2019 REAL CYH. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
